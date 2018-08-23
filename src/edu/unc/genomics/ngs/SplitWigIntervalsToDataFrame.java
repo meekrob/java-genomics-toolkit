8711536a26025d073472e5362a70f8c78a83dac3
David King <davidcking.inbox@gmail.com>
2018-08-23 11:06:07 -0600

    functional SplitWigIntervalsToDataFrame.java

--- src/edu/unc/genomics/ngs/SplitWigIntervalsToDataFrame.java
+++ src/edu/unc/genomics/ngs/SplitWigIntervalsToDataFrame.java
@@ -1,8 +1,11 @@
 package edu.unc.genomics.ngs;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.charset.Charset;
+import java.nio.file.*; // for Path, Paths, newBufferedWriter
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
 
 import org.apache.log4j.Logger;
 
@@ -39,6 +42,9 @@ public class SplitWigIntervalsToBedGraphPlus extends CommandLineTool {
 	
 	@Override
 	public void run() throws IOException {
+        log.debug("Initializing output file");
+        Charset charset = Charset.forName("US-ASCII");
+        BufferedWriter writer = Files.newBufferedWriter(outputFile, charset);
 		log.debug("Initializing input file");
 		int count = 0, skipped = 0;
 		try (
@@ -46,24 +52,22 @@ public class SplitWigIntervalsToBedGraphPlus extends CommandLineTool {
                IntervalFileReader<? extends Interval> intervals = IntervalFileReader.autodetect(lociFile) // no ending semicolon in try-with-resources block
             )
         {
-			log.debug("Iterating over all intervals and writing Wig for each");
-            //TrackHeader header = TrackHeader.newWiggle();
+			log.debug("Iterating over all intervals and writing Wig min,mean,max,N for each");
+            int interval_i = 0;
 			for (Interval interval : intervals) {
-                System.out.printf("encountered interval: %s\n", interval);
+                log.info("encountered interval: " + interval);
                 try {
                     Contig query = wig.query(interval);
-                    /*header.setName(interval.getId());
-                    try (WigFileWriter writer = new WigFileWriter(outputFile, header)) {
-                      writer.write(query);
-                    }*/
                     float[] values = query.getValues();
-                    //System.out.printf("interval %1$s has %2$d datapoints%n", interval, values.length);
                     final String chr = interval.getChr();
-                    final String id = interval.getId();
+                    String id = interval.getId();
+                    if (id == null) {
+                        id = "interval_" + interval_i;
+                    }
                     float i_max = Float.MIN_VALUE;
                     float i_min = Float.MAX_VALUE;
                     float i_sum = 0;
-                    float i_N = 0;
+                    int i_N = 0;
                     for (int startPos = interval.low(); startPos < interval.high(); startPos++) {
                         int i = startPos - interval.low();
                         i_max = Math.max(i_max, values[i]);
@@ -73,9 +77,11 @@ public class SplitWigIntervalsToBedGraphPlus extends CommandLineTool {
                     }
                     float i_mean = i_sum / i_N;
                     int startPos = interval.low();
-                    System.out.printf("%1$s\t%2$d\t%3$d\t%4$f\t%5$f\t%6$f\t%7$s\n", chr, startPos, startPos+1, i_min, i_mean, i_max, i_N, id); 
+                    String line = String.format("%1$s\t%2$d\t%3$d\t%4$f\t%5$f\t%6$f\t%7$d\t%8$s\n", chr, startPos, startPos+1, i_min, i_mean, i_max, i_N, id);
+                                                                                                  /* 1      2          3        4        5      6     7    8 */
+                    writer.write(line);
                     
-            
+                    interval_i++; 
                 } catch (WigFileException e) {
                     log.info("Skipping interval "+interval+" which has no data");
                     skipped++;
@@ -83,7 +89,8 @@ public class SplitWigIntervalsToBedGraphPlus extends CommandLineTool {
             count++;
             }
 		}
-		
+        writer.flush();
+        writer.close();		
 		log.info(count + " intervals processed");
 		log.info(skipped + " interval skipped");
 	}
